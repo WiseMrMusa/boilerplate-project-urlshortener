3a37006d79ad664ff8cbd2b084817877f4d9f9f2
WiseMrMusa <abdulkareemusa01@gmail.com>
2022-06-28 07:09:40 +0100

    test

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -18,6 +22,30 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+shortLinks = []
+
+app.post('/api/shorturl/',function(req,res){
+  var rawURL = req.body.url;
+
+  try {
+    url = new URL(rawURL);
+    var short = shortLinks.length;
+    shortLinks.push({"original_url":req.body.url,"short_url":short})
+    res.json({"original_url":req.body.url,"short_url":short})
+  } catch (_) {
+    return res.json({"error":"Invalid URL"})
+  }
+
+
+})
+
+app.get('/api/shorturl/:shorturl/',function(req,res){
+  var short = Number(req.params.shorturl);
+  // console.log(Number(req.params.shorturl),shortLinks,shortLinks[short],shortLinks[short].original_url)
+  res.redirect(shortLinks[short].original_url);
+  res.end();
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
